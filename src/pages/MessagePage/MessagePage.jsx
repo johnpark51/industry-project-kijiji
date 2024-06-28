10faf5ca3b977918b2696361ff9244fe045b1030
John Park <johnparkbluejay@gmail.com>
2024-06-28 11:40:30 -0400

    Message page added

---
+++ src/pages/MessagePage/MessagePage.jsx
@@ -0,0 +1,17 @@
+import "./MessagePage.scss";
+
+function MessagePage() {
+	return (
+		<div id="container">
+			<div className="message-page__left">
+                <p>Back to My Messages</p>
+				<h1>Put Message box component here</h1>
+			</div>
+			<div className="message-page__right">
+				<h1>Put User Profile component here</h1>
+			</div>
+		</div>
+	);
+}
+
+export default MessagePage;
