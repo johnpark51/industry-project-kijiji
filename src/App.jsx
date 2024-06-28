c2af95be5ea0e6f07538629d6fc725d62f47ea46
John Park <johnparkbluejay@gmail.com>
2024-06-27 23:53:39 -0400

    develop branch created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
+					<Route path="/" element={<HomePage />}></Route>
 				</Routes>
 			</BrowserRouter>
 		</>
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import "./HomePage.scss";
+
+function HomePage() {
+	return (
+		<>
+			<main>
+				<h1>Kijij</h1>
+			</main>
+		</>
+	);
+}
+
+export default HomePage;
