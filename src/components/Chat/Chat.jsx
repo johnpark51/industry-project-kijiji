55999c8776fcd245ba7ab6a00f5ca74c151ac24b
Jay <janghyeongjae@Jays-MacBook-Pro-2.local>
2024-06-28 14:59:41 -0400

    last commit

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,69 +1,70 @@
-30;
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import backArrow from "../../assets/images/chevron_right-24px.svg";
-import lama from "../../assets/images/lama.jpeg";
+import lama from "../../assets/images/download.jpeg";
 import "./Chat.scss";
 const socket = io("http://localhost:3001");
 const Chat = () => {
-	const [messages, setMessages] = useState([]);
-	const [input, setInput] = useState("");
-	const [userId] = useState(uuidv4());
-	useEffect(() => {
-		socket.on("chat message", (msg) => {
-			setMessages((prevMessages) => [...prevMessages, msg]);
-		});
-		return () => {
-			socket.off("chat message");
-		};
-	}, []);
-	const sendMessage = (e) => {
-		e.preventDefault();
-		if (input.trim()) {
-			socket.emit("chat message", { userId, text: input });
-			setInput("");
-		}
-	};
-	return (
-		<div className="message-page__left">
-			<div className="message-page__left--back">
-				<img src={backArrow} alt="back arrow" />
-				<p className="back-msg">Back to My Messages</p>
-			</div>
-			<div className="message-page__left--chat">
-				<div className="message-page__left--chat--header">
-					<img src={lama} alt="lama" className="lama" />
-					<div className="header-text">
-						<p className="header-text--name">Cute Lama</p>
-						<p className="header-text--price">$5.00</p>
-					</div>
-				</div>
-				<div className="chat-container">
-					<ul id="messages" className="messages-box">
-						{messages.map((msg, index) => (
-							<li
-								key={index}
-								className={`message ${
-									msg.userId === userId ? "sender" : "receiver"
-								}`}>
-								{msg.text}
-							</li>
-						))}
-					</ul>
-					<form onSubmit={sendMessage}>
-						<input
-							id="input"
-							value={input}
-							className="input-text"
-							onChange={(e) => setInput(e.target.value)}
-							autoComplete="off"
-							onKeyPressdown={(e) => e.key === "Enter" && sendMessage(e)}
-						/>
-					</form>
-				</div>
-			</div>
-		</div>
-	);
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
+  const [userId] = useState(uuidv4());
+  useEffect(() => {
+    socket.on("chat message", (msg) => {
+      setMessages((prevMessages) => [...prevMessages, msg]);
+    });
+    return () => {
+      socket.off("chat message");
+    };
+  }, []);
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (input.trim()) {
+      socket.emit("chat message", { userId, text: input });
+      setInput("");
+    }
+  };
+  return (
+    <div className="message-page__left">
+      <Link to="/" className="message-page__left--back">
+        <img src={backArrow} alt="back arrow" />
+        <p className="back-msg">Back to My Messages</p>
+      </Link>
+      <div className="message-page__left--chat">
+        <div className="message-page__left--chat--header">
+          <img src={lama} alt="lama" className="lama" />
+          <div className="header-text">
+            <p className="header-text--name">2008 Mazdar 3 3000miles</p>
+            <p className="header-text--price">$5.00</p>
+          </div>
+        </div>
+        <div className="chat-container">
+          <ul id="messages" className="messages-box">
+            {messages.map((msg, index) => (
+              <li
+                key={index}
+                className={`message ${
+                  msg.userId === userId ? "sender" : "receiver"
+                }`}
+              >
+                {msg.text}
+              </li>
+            ))}
+          </ul>
+          <form onSubmit={sendMessage}>
+            <input
+              id="input"
+              value={input}
+              className="input-text"
+              onChange={(e) => setInput(e.target.value)}
+              autoComplete="off"
+              onKeyPressdown={(e) => e.key === "Enter" && sendMessage(e)}
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Chat;
--- src/components/ReportModal/ReportModal.jsx
+++ src/components/ReportModal/ReportModal.jsx
@@ -4,69 +4,74 @@ import { useState } from "react";
 import { createPortal } from "react-dom";
 
 function ReportModal({ open, onClose }) {
-	const [openFeedbackModal, setOpenFeedbackModal] = useState(false);
+  const [openFeedbackModal, setOpenFeedbackModal] = useState(false);
 
-	if (!open) return null;
+  if (!open) return null;
 
-	return (
-		<>
-			{createPortal(
-				<Feedback
-					open={openFeedbackModal}
-					onClose={() => setOpenFeedbackModal(false)}
-				/>,
-				document.body
-			)}
-			<div onClick={onClose} className="overlay" data-aos="fade-down">
-				<div
-					className="modal"
-					onClick={(e) => {
-						e.stopPropagation();
-					}}>
-					<h5 className="modal__header">
-						Are you sure you want to report this user?
-					</h5>
-					<div className="modal__list">
-						<input type="checkbox" className="modal__checkbox" />
-						<p className="modal__check-text">User did not pay for the item</p>
-					</div>
-					<div className="modal__list">
-						<input type="checkbox" className="modal__checkbox" />
-						<p className="modal__check-text">User sent threatening language</p>
-					</div>
-					<div className="modal__list">
-						<input type="checkbox" className="modal__checkbox" />
-						<p className="modal__check-text">
-							User did not show up during hand off of item
-						</p>
-					</div>
-					<div className="modal__list">
-						<input type="checkbox" className="modal__checkbox" />
-						<p className="modal__check-text">
-							User is attempting to scam me with messages or links
-						</p>
-					</div>
-					<div className="modal__list">
-						<input type="checkbox" className="modal__checkbox" />
-						<p className="modal__check-text">Other</p>
-					</div>
-					<input
-						className="modal__input"
-						placeholder="Describe why you are reporting this user here"></input>
-					<div className="modal__buttons">
-						<button
-							onClick={onClose}
-							className="modal__button modal__button--cancel">
-							Cancel
-						</button>
-						<button                  
-							className="modal__button modal__button--report">
-							Report
-						</button>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      {createPortal(
+        <Feedback
+          open={openFeedbackModal}
+          onClose={() => setOpenFeedbackModal(false)}
+        />,
+        document.body,
+      )}
+      <div onClick={onClose} className="overlay" data-aos="fade-down">
+        <div
+          className="modal"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <h5 className="modal__header">
+            Are you sure you want to report this user?
+          </h5>
+          <div className="modal__list">
+            <input type="checkbox" className="modal__checkbox" />
+            <p className="modal__check-text">User did not pay for the item</p>
+          </div>
+          <div className="modal__list">
+            <input type="checkbox" className="modal__checkbox" />
+            <p className="modal__check-text">User sent threatening language</p>
+          </div>
+          <div className="modal__list">
+            <input type="checkbox" className="modal__checkbox" />
+            <p className="modal__check-text">
+              User did not show up during hand off of item
+            </p>
+          </div>
+          <div className="modal__list">
+            <input type="checkbox" className="modal__checkbox" />
+            <p className="modal__check-text">
+              User is attempting to scam me with messages or links
+            </p>
+          </div>
+          <div className="modal__list">
+            <input type="checkbox" className="modal__checkbox" />
+            <p className="modal__check-text">Other</p>
+          </div>
+          <input
+            className="modal__input"
+            placeholder="Describe why you are reporting this user here"
+          ></input>
+          <div className="modal__buttons">
+            <button
+              onClick={onClose}
+              className="modal__button modal__button--cancel"
+            >
+              Cancel
+            </button>
+            <button
+              className="modal__button modal__button--report"
+              onClick={onClose}
+            >
+              Report
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default ReportModal;
